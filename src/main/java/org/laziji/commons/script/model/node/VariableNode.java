230d70fa2f0adabf63c079418afe1470cadf5d14
GitHub-Laziji <c_smtp@163.com>
2020-07-04 00:30:35 +0800

    init

--- src/main/java/org/laziji/commons/script/model/node/VariableNode.java
+++ src/main/java/org/laziji/commons/script/model/node/VariableNode.java
@@ -3,25 +3,22 @@ package org.laziji.commons.script.model.node;
 import org.laziji.commons.script.exception.CompileException;
 import org.laziji.commons.script.exception.RunException;
 import org.laziji.commons.script.model.value.Value;
+import org.laziji.commons.script.model.value.VariableValue;
 
 import java.util.List;
 import java.util.Map;
 
-public class VariableDefinitionNode extends BaseNode {
+public class VariableNode extends BaseNode {
 
-    private VariableNode variableNode;
+    private VariableValue variableValue;
 
-    public VariableDefinitionNode(String segment) {
+    public VariableNode (String segment) {
         super(segment);
     }
 
     @Override
     public void compile() throws CompileException {
-        if (!segment.startsWith("let ")) {
-            throw new CompileException("Expect 'let' got '%s'", segment.substring(0, Math.min(segment.length(), 4)));
-        }
-        this.variableNode = new VariableNode(segment.substring(3));
-        this.variableNode.compile();
+
     }
 
     @Override
